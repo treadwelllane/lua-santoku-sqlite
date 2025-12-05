02417d1b0338875f6c8522014363bda82cc3c773
broma0 <121064585+broma0@users.noreply.github.com>
2025-12-05 18:20:18 -0500

    update dependencies and formatting

--- make.lua
+++ make.lua
@@ -1,23 +1,18 @@
 local env = {
-
   name = "santoku-sqlite",
-  version = "0.0.26-1",
+  version = "0.0.27-1",
   variable_prefix = "TK_SQLITE",
   license = "MIT",
   public = true,
-
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.246-1",
+    "santoku >= 0.0.303-1",
   },
-
   test = {
     dependencies = {
-      "luacov >= 0.15.0-1",
       "lsqlite3 >= 0.9.5",
     }
   },
-
 }
 
 env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
@@ -25,7 +20,5 @@ env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
 return {
-  
   env = env,
 }
-
